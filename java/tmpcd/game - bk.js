b0d810ff0053f229c609c9ec4ee2789304389da2
gurpreet5288 <gurpreetmca1988@gmail.com>
2020-03-25 16:17:11 -0700

    game code updated

--- java/tmpcd/game - bk.js
+++ java/tmpcd/game - bk.js
@@ -8,12 +8,8 @@ document.body.appendChild(canvas);
 var CheesesCaught = 0;
 var keysDown = {};
 var gameover=false; 
-var count = 50; 
-var ItemArr = [];
-const stepMax = 8;
-const stepMin = 3;  
-var BulletCaught = 0; 
-var TotalBulletsFire = 1;
+var count = 5;
+
 
 class Cheese{
     constructor(){  
@@ -116,81 +112,12 @@ class keyboardcontrols{
 }
 
 
-class Item{
-  constructor(){   
-      this.bulletImage = new Image(); 
-      this.bulletImage.src = "images/bullet.png"; 
-      this.x = 32 + (Math.random() * (canvas.width - 64));
-      this.y = 0;  
-      this.stepSize = Math.floor(Math.random() * (stepMax - stepMin + 1)) + stepMin;
-       
-  }
-  moveDown (){  
-      if(this.y >= canvas.height   ){  
-        this.x = 32 + (Math.random() * (canvas.width - 64));
-        this.y = 0 ;   
-      }else{
-        this.y += this.stepSize; 
-        
-
-            let iX =  this.x;
-            let iY =  this.y;
-          //  alert(iX +'----'+iY); 
-            if (
-              Rat_obj.x <= (iX  + 15)
-              && iX <= (Rat_obj.x  + 15)
-              && Rat_obj.y <= (iY  + 15)
-              && iY <= (Rat_obj.y  + 15)
-            ) { 
-              this.y += 50 ; 
-              ++BulletCaught;
-              Cheese_obj.reset();
-              Rat_obj.reset();
-              if(BulletCaught >= 10){
-                  gameover = true;
-                  alert(BulletCaught);
-              } 
-            }
-      }
-  }
-  drawItem(){ 
-      //ctx.fillRect(this.xPos ,this.yPos,this.size,this.size);
-      ctx.drawImage(this.bulletImage, this.x, this.y); 
-  }
-  crash(){  
-    if (
-      Rat_obj.x <= (Cheese_obj.x + 32)
-      && Cheese_obj.x <= (Rat_obj.x + 32)
-      && Rat_obj.y <= (Cheese_obj.y + 32)
-      && Cheese_obj.y <= (Rat_obj.y + 32)
-    ) {
-      ++CheesesCaught;
-      Cheese_obj.reset();
-      Rat_obj.reset();
-    }
-}
-
-} 
-
-class Bullet extends Item{
-    constructor(){
-      super("green"); 
-      /*this.backImage= new Image(); 
-      this.backImage.src = "images/bullet.png";*/
-    }
-    /*
-    drawItem(){
-        ctx.fillStyle  = this.color;
-        ctx.fillRect(this.xPos ,this.yPos,this.size,this.size);
-    }*/
-}
-
 
 class Game {  
 
     constructor( ){
         this.backImage= new Image(); 
-        this.backImage.src =    "images/background.png";
+        this.backImage.src = "images/background.png";
     }
 
     // Draw everything on the canvas
@@ -205,7 +132,6 @@ class Game {
           ctx.textBaseline = "top";
           ctx.fillText("Cheeses caught: " + CheesesCaught, 20, 20); 
           ctx.fillText("Time: " + count, 20, 50);
-          ctx.fillText("Bullet caught: " + BulletCaught, 20, 70); 
           if(gameover){ 
             ctx.fillText("Game over!", 200, 220);
           }
@@ -218,52 +144,21 @@ class Game {
     } 
         
     // The main game loop
-    main(){ 
-        
-
+    main(){  
           game.drawbg(); 
-        
           Rat_obj.update(0.02);
           controls.crash();  
           controls.init(); 
-          
-           
-              
-         
-            // clearScreen();
-         
           // run the update function
           /* update(0.02); // do not change
           // run the render function*/
           game.render();
-          
           // Request to do this again ASAP
           requestAnimationFrame(game.main);
-          if(!gameover){
-            requestAnimationFrame(game.rmain);
-          }else{
-            ItemArr ='';
-          }
-            
-    }
-    rmain(){
-       // setInterval(() => { 
-          for ( let i=0;i < TotalBulletsFire; i++){ 
-              ItemArr.push (new Bullet());   
-              ItemArr[i].drawItem();
-              ItemArr[i].moveDown();  
-              
-
-          } 
-            controls.crash(); 
-       // },1000); 
     }
 
 }
-const clearScreen  = ()  => {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  
-}
+
 var counter =function(){
         count=count-1; // countown by 1 every second
         // when count reaches 0 clear the timer, hide Cheese and
@@ -274,11 +169,6 @@ var counter =function(){
           // set game to finished
           gameover = true;
           count=0; 
-        }   
-        if(count % 5 == 0) {
-          TotalBulletsFire +=1;
-        }else{
-         // alert(count);
         }
 }
 setInterval(counter, 1000);
@@ -300,7 +190,7 @@ let game = new Game();
 
 Cheese_obj.reset();    
 Rat_obj.reset();    
-game.main();  
+game.main();
  // timer interval is every second (1000ms)
 
 
